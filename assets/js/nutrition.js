cb4e8c6ed262f81821480f92be018802d67025b3
JoshuaBentley <joshuabentley911@gmail.com>
2022-12-22 12:56:32 -0700

    was going to work on the nutrition API but was missing all JS so added new API and have it set to read inputed data and display results but i could not get a BMR api so it is doing BMI for now

---
+++ assets/js/nutrition.js
@@ -0,0 +1,28 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9aba823730msh22aa7df619887b2p1b55fdjsn6329039a1d9c',
+		'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+	}
+};
+
+var showResults = document.querySelector('button')
+showResults.addEventListener('click', getResults)
+
+function getResults(){
+    var weight = document.getElementById('weight').value;
+    var height = document.getElementById('height').value;
+    // var age = document.getElementById('age').value;
+    var bmr = document.getElementById('bmr');
+    fetch('https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=' + weight + '&height=' + height, options)
+    // fetch('https://bmr-and-tmr.p.rapidapi.com/calculate-bmr?weight=' + weight + '&height=' + height + '&age=' + age + '&sex=male&inImperial=false', options)
+	.then(response => response.json())
+	.then(response => bmr.innerText = response.bmi + "your BMI")
+    .then(response => console.log(response) )
+	.catch(err => console.error(err));
+   
+}
+
+
+
+
