af57a89602df8618a48f92f39a749f936e9a6a4d
JoshuaBentley <joshuabentley911@gmail.com>
2022-12-27 18:54:05 -0700

    can now search for a recipe and have a reuslt shown

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,18 +1,16 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9aba823730msh22aa7df619887b2p1b55fdjsn6329039a1d9c',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
-};
 
-var showResults = document.querySelector('.searchButton')
-showResults.addEventListener('click', getFood)
-
-function getFood() {
-	var food = 'beef'
-	fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-}
+$('.searchButton').click(function workout() {
+	var recipe = $('input').val()
+	$.ajax({
+    	method: 'GET',
+    	url: 'https://api.api-ninjas.com/v1/recipe?query=' + recipe,
+    	headers: { 'X-Api-Key': '+qJeMFVwgk8PQfrADoeAFw==OoEPXG8KEs97zdh6'},
+    	contentType: 'application/json',
+    	success: function(result) {
+        	console.log(result);
+			$('.recipe1').text(result[0].ingredients)
+    	},
+    	error: function ajaxError(jqXHR) {
+        	console.error('Error: ', jqXHR.responseText);
+    	}
+})})
